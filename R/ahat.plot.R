644a6d4ab0ff8c8bdb21bd086cb3ac74f62011e7
foramashar <foramashar@gmail.com>
2015-03-30 11:43:09 -0400

    GRS commit

---
+++ R/ahat.plot.R
@@ -0,0 +1,50 @@
+#' Function to make the Ahat plot for genetic enrichment analyses
+#' Builds on grs.summary function from package gtx
+#' 
+#' @param x input data frame of results from grs.summary at different Pval cutoffs
+#' @param genderdiffvector optional list of pvalue for difference in ahat by sex
+#' 
+#' @export
+#'
+#' @seealso gtx, grs.plot
+#' @examples
+#' with(test, grs.plot.new(get(paste("Effect",trait, sep=".")), AdjustedSCD.Effect, StdErr,Freq1, col.by="af"))
+
+function (x, genderdiffvector ){
+	test=as.data.frame(t(x))
+	test$group=gsub("\\..+","",row.names(test))
+	ylimmax=max(test$ahat+qnorm(1-0.05/2)*test$aSE)
+	testcombined=with(subset(test,group=="combined"), rbind(m, prettyNum(ahat, digits=2), prettyNum(R2rs, digits=2),  prettyNum(pval, digits=3),prettyNum(phet, digits=3)))
+	if ("male" %in% test$group) {
+		testmale=with(subset(test,group=="male"), rbind(m, prettyNum(ahat, digits=2), prettyNum(R2rs, digits=2),  prettyNum(pval, digits=3),prettyNum(phet, digits=3)))
+		testfemale=with(subset(test,group=="female"), rbind(m, prettyNum(ahat, digits=2), prettyNum(R2rs, digits=2),  prettyNum(pval, digits=3),prettyNum(phet, digits=3)))
+	}
+	testgenderdiff=as.matrix(round(genderdiffvector,3))
+	test.table=rbind(testcombined, testmale, testfemale,testgenderdiff)
+	row.names(test.table)=c(rep(c("m","ahat","R2rs","pval","phet"),3), "GenderP")
+	colnames(test.table)=gsub(paste("combined",trait,"scd.", sep="."),"",row.names(subset(test[order(test$m),], group=="combined")))
+	with(subset(test[order(test$m),], group=="combined"), {
+		axislabels=gsub(paste("combined",trait,"scd.", sep="."),"",row.names(subset(test[order(test$m),], group=="combined")))
+		plot(ahat, pch=19, col="forestgreen", ylim=ylimtable[,trait],xlim=c(0.5,length(axislabels)+1),xlab="Alpha Cutoff", cex.lab=1.2,xaxt="n",main=paste("GRS--",toupper(trait),"in SCD"))
+		axis(1,at=(1:length(axislabels))+0.2,labels=axislabels)
+		for (i in 1:length(axislabels)) {
+			lines(cbind(rep(i, 2), ahat[i] + qnorm(c(0.05/2, 1-0.05/2)) * aSE[i]), col = "grey")
+		}
+	})
+	abline(h=0, lty="dotted",col="grey")
+	with(subset(test[order(test$m),],group=="male"), {
+		axislabels=gsub(paste("male",trait,"scd.", sep="."),"",row.names(subset(test[order(test$m),], group=="male")))
+		points((1:length(axislabels))+0.2,ahat, pch=19, col="blue")
+		for (i in 1:length(axislabels)) {
+			lines(cbind(rep(i+0.2, 2), ahat[i] + qnorm(c(0.05/2, 1-0.05/2)) * aSE[i]), col = "grey")
+		}
+	})
+	with(subset(test[order(test$m),],group=="female"), {
+		axislabels=gsub(paste("female",trait,"scd.", sep="."),"",row.names(subset(test[order(test$m),], group=="female")))
+		points((1:length(axislabels))+0.4,ahat, pch=19, col="deeppink")
+		for (i in 1:length(axislabels)) {
+			lines(cbind(rep(i+0.4, 2), ahat[i] + qnorm(c(0.05/2, 1-0.05/2)) * aSE[i]), col = "grey")
+		}
+	})
+	addtable2plot.new("bottom", table=test.table, display.rownames=T,display.colnames=T, hlines=T, bg=c(rep(c("forestgreen","blue","deeppink"),each=(nrow(test.table)-1)/3),"white"), text.col= c(rep("white", (nrow(test.table)-1)),"black"), cex=0.9, xpad=0.5)
+}
