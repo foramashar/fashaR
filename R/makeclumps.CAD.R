1c69009bfce5f5548bf2d969a7e2a298cff88fe4
foramashar <foramashar@gmail.com>
2015-06-22 19:24:04 -0400

    mito commit

--- R/makeclumps.CAD.R
+++ R/makeclumps.CAD.R
@@ -5,7 +5,7 @@
 #' @export
 #' @seealso gtx, grs.plot
 
-makeclumps=function(trait){
+makeclumps.CAD=function(trait){
 i=5e-8
 	system(paste("plink2 --bfile /dcs01/arking/arkinglab/active/projects/scd.meta/analyses/scd.meta.ver2/ARIC.b35.b37.liftover/aric.f3v2.imputed.b37 --clump ", paste("CAD.",trait,".overlap.SNP.MIpval",sep="")," --clump-p1 ",format(i, scientific=F)," --clump-p2 0.05 --clump-r2 ",ifelse(i==5e-8, 0, 0.05)," --clump-kb 1000 --clump-field pvalue --clump-verbose --out ",trait,".cad.",format(i, scientific=F), sep=""))
 		clumpedname=paste(trait,".cad.",format(i, scientific=F),".clumped", sep="")
