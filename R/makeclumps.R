06286ea8d125585b9d7f2058ef57b81db23ba226
foramashar <foramashar@gmail.com>
2015-07-20 14:21:53 -0400

    SCD TAG commit

--- R/makeclumps.R
+++ R/makeclumps.R
@@ -18,6 +18,10 @@ i=5e-8
 		system(paste("mv test2 	", intervalname, sep=""))
 
 	for (i in c(1e-5,1e-3, 0.05, 0.99)){
-		system(paste("plink2 --bfile /dcs01/arking/arkinglab/active/projects/scd.meta/analyses/scd.meta.ver2/ARIC.b35.b37.liftover/aric.f3v2.imputed.b37 --exclude range ", intervalname," --clump ", paste("SCD.",trait,".overlap.SNP.MIpval",sep=""),", --clump-p1 ",format(i, scientific=F)," --clump-p2 0.99 --clump-r2 0.05 --clump-kb 500 --clump-field pvalue --out ",trait,".scd.",format(i, scientific=F), sep=""))
+		if (as.numeric(system(paste("wc -l", intervalname)))==0){
+		 system(paste("plink2 --bfile /dcs01/arking/arkinglab/active/projects/scd.meta/analyses/scd.meta.ver2/ARIC.b35.b37.liftover/aric.f3v2.imputed.b37 --clump ", paste("SCD.",trait,".overlap.SNP.MIpval",sep=""),", --clump-p1 ",format(i, scientific=F)," --clump-p2 0.99 --clump-r2 0.05 --clump-kb 500 --clump-field pvalue --out ",trait,".scd.",format(i, scientific=F), sep=""))
+		} else {
+			system(paste("plink2 --bfile /dcs01/arking/arkinglab/active/projects/scd.meta/analyses/scd.meta.ver2/ARIC.b35.b37.liftover/aric.f3v2.imputed.b37 --exclude range ", intervalname," --clump ", paste("SCD.",trait,".overlap.SNP.MIpval",sep=""),", --clump-p1 ",format(i, scientific=F)," --clump-p2 0.99 --clump-r2 0.05 --clump-kb 500 --clump-field pvalue --out ",trait,".scd.",format(i, scientific=F), sep=""))
+		}
 	}
 }
