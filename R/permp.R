4b1807a80ef750d56c215b3863b4d4ce17e355d4
foramashar <foramashar@gmail.com>
2015-11-13 10:18:21 -0500

    scd commit

--- R/permp.R
+++ R/permp.R
@@ -2,13 +2,14 @@
 #'
 #' @param testnull Vector of null pval distribution
 #' @param testdata Vector of pval from dataset of interest
+#' @param numperm Numeric value of how many permutations were done
+#'        Default value 1000
 #' @export
 #'
 #' @return output Vector of permuted pvalues
 #'
 
-permp=function(testnull, testdata){
-	numperm=length(testnull)
+permp=function(testnull, testdata, numperm=1000){
 	testnull=na.omit(testnull)
 	test=sapply(testdata, function(x){
 		if (is.na(x)){
