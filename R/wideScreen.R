679c3e18b5d8af9316e7110ee93b3e72e2e613a1
foramashar <foramashar@gmail.com>
2016-01-29 10:50:04 -0500

    scd commit

---
+++ R/wideScreen.R
@@ -0,0 +1,8 @@
+#' Function to increase width of R on linux
+#'
+#' @return output Width of the window is changed
+#'
+
+wideScreen <- function(howWide=as.numeric(strsplit(system('stty size', intern=T), ' ')[[1]])[2]) {
+   options(width=as.integer(howWide))
+}
