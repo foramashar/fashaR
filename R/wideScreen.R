8d1013b8222ea29b179ef7e58c61905923b61136
foramashar <foramashar@gmail.com>
2016-01-29 10:56:57 -0500

    scd commit

--- R/wideScreen.R
+++ R/wideScreen.R
@@ -3,6 +3,6 @@
 #' @return output Width of the window is changed
 #'
 
-wideScreen <- function(howWide=as.numeric(strsplit(system('stty size', intern=T), ' ')[[1]])[2]) {
+wideScreen=function(howWide=as.numeric(strsplit(system('stty size', intern=T), ' ')[[1]])[2]) {
    options(width=as.integer(howWide))
 }
